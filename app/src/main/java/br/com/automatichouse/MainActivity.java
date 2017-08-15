c265a2b5a944f25b41030a81e747fe3fdb7d36e2
Fellipe Soares <fellipe_so@hotmail.com>
2017-08-14 21:41:37 -0300

    first commit

---
+++ app/src/main/java/br/com/automatichouse/MainActivity.java
@@ -0,0 +1,144 @@
+package br.com.automatichouse;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Environment;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button play, stop, record;
+    private MediaRecorder myAudioRecorder;
+    private String outputFile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        play = (Button) findViewById(R.id.play);
+        stop = (Button) findViewById(R.id.stop);
+        record = (Button) findViewById(R.id.record);
+        stop.setEnabled(false);
+        play.setEnabled(false);
+
+        outputFile = Environment.getExternalStorageDirectory().getAbsolutePath() + "/recording.3gp";
+
+        myAudioRecorder = new MediaRecorder();
+        myAudioRecorder.reset();
+        myAudioRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+        myAudioRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+        myAudioRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB);
+        myAudioRecorder.setOutputFile(outputFile);
+
+        record.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    myAudioRecorder.prepare();
+                    myAudioRecorder.start();
+                } catch (IllegalStateException ise) {
+                    // make something ...
+                } catch (IOException ioe) {
+                    // make something
+                }
+
+                record.setEnabled(false);
+                stop.setEnabled(true);
+
+                Toast.makeText(getApplicationContext(), "Recording started", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    myAudioRecorder.stop();
+                }
+                catch (Exception e){
+                }
+                myAudioRecorder.release();
+                myAudioRecorder = null;
+                record.setEnabled(true);
+                stop.setEnabled(false);
+                play.setEnabled(true);
+                Toast.makeText(getApplicationContext(), "Audio Recorder successfully", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MediaPlayer mediaPlayer = new MediaPlayer();
+                AmazonS3 s3 = new AmazonS3Client();
+                TransferUtility transferUtility = new TransferUtility(s3, getApplicationContext());
+                TransferObserver observer = transferUtility.upload(
+                        MY_BUCKET,     /* The bucket to upload to */
+                        OBJECT_KEY,    /* The key for the uploaded object */
+                        MY_FILE        /* The file where the data to upload exists */
+                );
+                try {
+                    Log.d("SetDatasource path", outputFile);
+                    isStoragePermissionGranted();
+                    File filePath = new File(outputFile);
+
+                    if (!filePath.exists())
+                    {
+                        filePath.createNewFile();
+                    }
+
+                    FileInputStream is = new FileInputStream(filePath);
+
+                    mediaPlayer.setDataSource(is.getFD());
+                    mediaPlayer.prepare();
+                    mediaPlayer.start();
+
+                    Toast.makeText(getApplicationContext(), "Playing Audio", Toast.LENGTH_LONG).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+
+    public  boolean isStoragePermissionGranted() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    == PackageManager.PERMISSION_GRANTED) {
+                Log.d("Permission is granted", "");
+                return true;
+            } else {
+
+                Log.d("Permission is revoked", "");
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                return false;
+            }
+        }
+        else { //permission is automatically granted on sdk<23 upon installation
+            Log.d("Permission is granted", "");
+            return true;
+        }
+    }
+}
+
